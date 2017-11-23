2cdd2b3b140a2d908fbbe82be010e498450a9d44
craig-mulligan <craig@resin.io>
2017-11-23 16:23:47 +0000

    Add avahi-browse + use host dbus

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -e
+# set -e
 
 # Set host-name and enable-dbus
-sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
-
-systemctl stop avahi-daemon &&
-
-avahi-daemon
+# sed -i -e "s@#enable-dbus=yes@enable-dbus=no@" -e "s@#host-name=foo@host-name=$(echo $RESIN_DEVICE_UUID | cut -c1-7)@" /etc/avahi/avahi-daemon.conf
+#
+# systemctl stop avahi-daemon &&
+#
+# avahi-daemon
